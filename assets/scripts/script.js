0cb89b8899fa841dc8a071fc0fb80c274bb0f518
rash <orashusedmund@gmail.com>
2022-12-21 18:37:37 +0100

    .'

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,71 @@
-const inputVallues = document.querySelector(".weights");
-const displayResults = document.querySelector(".display-results")
-const submitBtn = document.querySelector(".submit-btn")
 
-console.log(`this is inputVallues: ${inputVallues.value}\n`)
-console.log(`this is displayResults: ${displayResults.value}\n`)
+// submitBtn.addEventListener('click', () => {
+//   if((inputVallues.value == "")&&(displayResults.innerHTML == "")){
+//     alert('gran add Elements 1 and 2')
+//   } else {
+//     let arrVallues = weights.split(",")
+//     console.log(`arrVallues = ${arrVallues}`)
 
-submitBtn.addEventListener('click', () => {
-  if((inputVallues.value == "")&&(displayResults.innerHTML == "")){
-    alert('gran add Elements 1 and 2')
+//     let arrWeights = weights.split(",")
+//     console.log(`arrWeights = ${arrWeights}`)
+//   }
+// })
+
+const input1 = document.querySelector('#weights')
+const weightsToChose = document.querySelector('#list-0f-weights')
+const evaluateBtn = document.querySelector('#evaluate')
+const displayResults = document.querySelector('#display')
+let result
+
+evaluateBtn.addEventListener('click', (e) => {
+  console.log(input1.value)
+  if (input1.value === '') {
+    alert('Please input two weights to balance')
+  } else if (weightsToChose.value === '') {
+    alert('Please input optional weights to select from for scale balancing')
   } else {
-    let arrVallues = weights.split(",")
-    console.log(`arrVallues = ${arrVallues}`)
+    const elementoneArr = input1.value.split(',').map(Number)
+    const elementtwoArr = weightsToChose.value.split(',').map(Number)
+    console.log(elementoneArr)
+    console.log(elementtwoArr)
 
-    let arrWeights = weights.split(",")
-    console.log(`arrWeights = ${arrWeights}`)
+    if (elementoneArr.length !== 2 || elementoneArr[0] === 0) {
+      displayResults.innerHTML = (
+        'Weight added is less or more than two integers. Only two comma seperated weights can be added'
+      )
+    } else {
+    // the scale balancing is processed by calling the balanceScale function
+      const result2 = balanceScale(elementoneArr, elementtwoArr)
+      displayResults.innerHTML = result
+      return result2
+    }
   }
 })
+
+function balanceScale (elementone, elementtwo) {
+  const diff = ((elementone[1] - elementone[0])**2)**(1/2)
+  if (diff === 0) {
+    result = 'scale is balanced'
+    return result
+  }
+
+  for (let z = 0; z < elementtwo.length; z++) {
+    if (elementtwo[z] === diff) {
+      result = [elementtwo[z]]
+      return result
+    }
+  }
+  for (let i = 0; i < elementtwo.length; i++) {
+    for (let j = 1; j < elementtwo.length; j++) {
+      if (elementtwo[i] + elementtwo[j + 1] === diff) {
+        result = [elementtwo[j + 1], elementtwo[i]]
+      } else if ((elementtwo[i] + diff === elementtwo[j + 1])) {
+        result = [elementtwo[j + 1], elementtwo[i]]
+
+        return result
+      } else {
+        result = 'no number available to balance'
+      }
+    }
+  }
+}
